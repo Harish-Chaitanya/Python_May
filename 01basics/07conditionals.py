feccd220c9c8d5dadb0c7339ec4132d107d635be
akshitagarg275 <akshitagarg275@gmail.com>
2022-05-24 08:46:49 +0530

    conditionals

---
+++ 01basics/07conditionals.py
@@ -0,0 +1,80 @@
+'''
+only if
+if-else pair
+if elif ladder
+'''
+# TODO: only if
+# age = int(input("Enter your age : "))
+
+# if age>=18:
+#     print("You can drive")
+
+# TODO : if -else pair
+# age = int(input("Enter your age : "))
+
+# if age>=18:
+#     print("You can drive")
+# else:
+#     print("You cannot drive")
+
+# TODO: if-elif ladder
+# num = int(input("Enter a number : "))
+
+# if num > 0:
+#     print(num, " is positive")
+# elif num < 0:
+#     print(num , " is negative")
+# else:
+#     print("It is zero")
+
+
+# TODO : Feedback system (1-5)
+# TODO: grading system 
+
+
+# TODO: Nested if else
+'''
+if: 
+    if:
+        //if statements
+else:
+    if:
+        //if statements
+    else:
+        //else statements
+
+'''
+
+num1 = 5
+num2=8
+num3=12
+
+if num1 > num2:
+    if num1 > num3 : 
+        print("Num1 is greatest")
+    else:
+        print("Num3 is greatest")
+else:
+    if num2>num3:
+        print("Num2 is greatest")
+    else:
+        print("Num3 is greatest")
+
+
+'''
+# whether user is signed in or not
+is_google =True
+is_facebook=False
+is_email = True
+'''
+
+
+'''
+is_cart= True
+is_pay = True
+is_loggedin = True
+# Whether user can make purchase or not
+'''
+
+
+
