e92160587807a51714432612afc4a3344b358c7f
akshitagarg275 <akshitagarg275@gmail.com>
2022-05-25 08:36:11 +0530

    loops

--- 01basics/07conditionals.py
+++ 01basics/07conditionals.py
@@ -67,6 +67,35 @@ is_google =True
 is_facebook=False
 is_email = True
 '''
+is_google =True
+is_facebook=False
+is_email = True
+
+# if (is_google or is_facebook or is_email):
+#     print("You are signed in")
+# else:
+#     print("Please sign in")
+
+# if is_google:
+#     print("You are signed in")
+# elif is_email:
+#     print("You are signed in")
+# elif is_facebook:
+#     print("You are signed in")
+# else:
+#     print("Please signed in")
+
+
+# if is_google:
+#     print("Signed in")
+# else:
+#     if is_facebook:
+#         print("Signed in")
+#     else:
+#         if is_email:
+#             print("signed in")
+#         else:
+#             print("Please sign in")
 
 
 '''
@@ -75,6 +104,26 @@ is_pay = True
 is_loggedin = True
 # Whether user can make purchase or not
 '''
+
+is_cart= False
+is_pay = True
+is_loggedin = True
+
+# if (is_cart and is_pay and is_loggedin):
+#     print("order placed successfully")
+# else:
+#     print("Cannot place order")
+
+if is_loggedin:
+    if is_cart:
+        if is_pay:
+            print("Order placed successfully")
+        else:
+            print("cannot place order")
+    else:
+        print("cannot place order")
+else:
+    print("cannot place order")
 
 
 
---
+++ 01basics/08loops.py
@@ -0,0 +1,68 @@
+'''
+DRY -> Donot Repeat Yourself
+
+Loops
+while loop
+for loop
+'''
+
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+
+'''
+while loop
+initialization
+condition
+re-initialization(increment/decrement)
+'''
+
+# i=1
+
+# while i<=5: 
+#     print("Hello ",i)
+#     i=i+1
+
+# TODO : WAP to print reverse counting
+
+
+
+# i=5
+
+# while i>=1:
+#     print(i)
+#     i=i-1
+
+# TODO: WAP to print even numbers b/w 0-10
+
+# i = 0
+
+# while i<=10:
+#     print(i)
+#     i=i+2
+
+# i=0
+
+# while i<=10:
+#     if i%2 == 0:
+#         print(i)
+#     i=i+1
+
+
+# TODO : for loop
+# We execute for loop on iterables(list, string, tuples, dictionary,range)
+
+fruits= ["mango","guava","grapes","apple","kiwi"]
+
+# for i in fruits:
+#     print(i)
+
+# name="John"
+
+# for i in name:
+#     print(i)
+
+for num in range(5):
+    print(num)
